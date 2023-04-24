adf866d3bc54698a176119f8dfe234983f5f0a0a
roycemoses <royce.kmoses@gmail.com>
2023-04-24 09:07:49 -0700

    first commit

---
+++ src/test/java/doConnect/cogentcapstone/CogentCapstoneApplicationTests.java
@@ -0,0 +1,13 @@
+package doConnect.cogentcapstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CogentCapstoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
